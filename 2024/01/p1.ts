eef86d7eeaac041b9c0ce8f61d5a4b0075e016d7
Ouassim Abdelmalek Ghribi <ouassimghribi@gmail.com>
2024-12-02 09:30:05 +0100

    chore: refactor day 1 solution

--- 2024/01/p1.ts
+++ 2024/01/p1.ts
@@ -2,12 +2,13 @@ function aocd01p01(input: string) {
   const list1: number[] = [];
   const list2: number[] = [];
 
-  input.split(/\s+/).map((v, idx) => {
-    if (v !== "") {
+  input
+    .trim()
+    .split(/\s+/)
+    .map((v, idx) => {
       if (idx % 2 === 0) list1.push(parseInt(v));
       else list2.push(parseInt(v));
-    }
-  });
+    });
 
   if (list1.length !== list2.length)
     throw new Error("Input lists should be of the same length.");
